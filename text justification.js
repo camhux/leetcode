e74d81f4b35df202fc984f72e56e22cbbbc224a5
camhux <camzig@gmail.com>
2015-09-11 20:55:25 -0700

    Text justification

---
+++ text justification.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string[]} words
+ * @param {number} maxWidth
+ * @return {string[]}
+ */
+var fullJustify = function(words, maxWidth) {
+  var runChars = 0;
+  var runWords = []; 
+  var lines = [];
+  var lineChars = [];
+
+  for (var i = 0; i < words.length; i++) {
+    runWords.push(words[i]);
+    runChars += words[i].length;
+    if (!words[i+1] || (runChars + words[i+1].length + (runWords.length)) > maxWidth) {
+      lines.push(runWords);
+      lineChars.push(runChars);
+      runWords = [];
+      runChars = 0;
+    }
+  }
+  
+  lines = lines.map(function(line, i) {
+    if (line.length === 1) {
+      line = line[0];
+      while (line.length < maxWidth) {
+        line += " ";
+      }
+      return line;
+    }
+
+    var lineCharCt = lineChars[i];
+
+    var charDiff = maxWidth - lineCharCt;
+    var charDiffLeft = charDiff;
+    var sepCt = line.length - 1;
+    var sepSize = Math.ceil(charDiff / sepCt);
+
+    for (var i = 0; i < line.length - 1; i++) {
+      console.log(sepSize);
+      line[i] = line[i] + makeSeparator(sepSize);
+      charDiffLeft -= sepSize;
+      sepSize = Math.ceil(charDiffLeft / --sepCt);
+    }
+
+    return line.join('');
+  });
+
+  return lines.join('\n');
+};
+
+function makeSeparator(n) {
+  var output = "";
+  while (output.length < n) {
+    output += " ";
+  }
+  return output;
+}
+
+console.log(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16));
