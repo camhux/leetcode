d56cf2b6943c5c239badaa73c9ebe2bf6f809090
camhux <camzig@gmail.com>
2015-11-18 23:08:15 -0800

    Add benchmarks and a linear control version

--- sparse-sorted-search/sparse-sorted-search.go
+++ sparse-sorted-search/sparse-sorted-search.go
@@ -14,9 +14,74 @@ func Search(array []string, target string) int {
 		mid = (high-low)/2 + low
 
 		if array[mid] == "" {
-			var lBound, rBound int
-			lBound = mid
-			rBound = mid
+			lBound, rBound := mid, mid
+
+			for array[lBound] == "" && lBound > 0 {
+				lBound--
+			}
+
+			for array[rBound] == "" && rBound < n-1 {
+				rBound++
+			}
+
+			left := array[lBound]
+			right := array[rBound]
+
+			switch {
+			case left == target:
+				return lBound
+			case right == target:
+				return rBound
+			case left > target:
+				high = lBound - 1
+				continue
+			case right < target:
+				low = rBound + 1
+				continue
+			default:
+				break
+			}
+		}
+
+		switch s := array[mid]; {
+		case s == target:
+			return mid
+		case s > target:
+			high = mid - 1
+			continue
+		case s < target:
+			low = mid + 1
+			continue
+		}
+	}
+
+	return -1
+}
+
+func SearchLinear(array []string, target string) int {
+
+	for i, string := range array {
+		if string == target {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func SearchConcurrent(array []string, target string) int {
+	var n, low, mid, high int
+
+	n = len(array)
+	low = 0
+	high = n - 1
+
+	for low < high {
+
+		mid = (high-low)/2 + low
+
+		if array[mid] == "" {
+			lBound, rBound := mid, mid
 
 			var wg sync.WaitGroup
 
--- sparse-sorted-search/sparse-sorted-search_test.go
+++ sparse-sorted-search/sparse-sorted-search_test.go
@@ -1,6 +1,10 @@
 package sparse
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 type Test struct {
 	array    []string
@@ -41,10 +45,95 @@ var cases = []Test{
 	},
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func TestSearch(t *testing.T) {
 	for _, test := range cases {
 		if actual := Search(test.array, test.target); actual != test.expected {
 			t.Errorf("Wrong index from search. Expected %v, got %v", test.expected, actual)
 		}
 	}
+}
+
+func TestSearchLinear(t *testing.T) {
+	for _, test := range cases {
+		if actual := SearchLinear(test.array, test.target); actual != test.expected {
+			t.Errorf("Wrong index from linear search. Expected %v, got %v", test.expected, actual)
+		}
+	}
+}
+
+func TestSearchConcurrent(t *testing.T) {
+	for _, test := range cases {
+		if actual := SearchConcurrent(test.array, test.target); actual != test.expected {
+			t.Errorf("Wrong index from concurrent search. Expected %v, got %v", test.expected, actual)
+		}
+	}
+}
+
+var benchmarkCase = genBenchmarkCase()
+
+func BenchmarkSearch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Search(benchmarkCase.array, benchmarkCase.target)
+	}
+}
+
+func BenchmarkSearchLinear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SearchLinear(benchmarkCase.array, benchmarkCase.target)
+	}
+}
+
+func BenchmarkSearchConcurrent(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SearchConcurrent(benchmarkCase.array, benchmarkCase.target)
+	}
+}
+
+func genBenchmarkCase() Test {
+	out := make([]string, 10000)
+	target := genRandomStringN(3)
+	targetIdx := rand.Intn(10001)
+
+	out[targetIdx] = target
+
+	for i := range out {
+		if out[i] == "" && rand.Intn(10) < 2 {
+			out[i] = genRandomStringN(2)
+		}
+	}
+
+	return Test{
+		out,
+		target,
+		targetIdx,
+	}
+}
+
+const letters = "abcdefghijklmnopqrstuvwxyz"
+const (
+	letterIdxBits = 5
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+func genRandomStringN(n int) string {
+	out := make([]byte, n)
+
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			out[i] = letters[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(out)
 }
